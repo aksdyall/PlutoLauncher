e3265d8c7aaed7e805e87f4a15e67a06f3f7e6d0
Alexander Dyall <39445611+aksdyall@users.noreply.github.com>
2026-06-30 13:04:53 -0400

    Modify Pluto installation logic
    Check if Pluto is installed before adding or updating.

--- .PlutoLauncher/PlutoLauncherDE.jl
+++ .PlutoLauncher/PlutoLauncherDE.jl
@@ -4,9 +4,18 @@
 launch_directory="~/Documents/"
 
 using Pkg
-Pkg.add("Pluto")
-Pkg.update("Pluto")
-cd(expanduser(launch_directory))
+pluto_is_installed=false
+for d in values(Pkg.dependencies())
+    if d.name=="Pluto"
+        global pluto_is_installed = true
+        break
+    end
+end
+if pluto_is_installed
+    Pkg.update("Pluto")
+else
+    Pkg.add("Pluto")
+end
 using Pluto
 Pluto.run()
 
