f6aab78d19ca5919ef180dfa6d74b5f020683206
Alexander Dyall <39445611+aksdyall@users.noreply.github.com>
2026-06-30 13:06:19 -0400

    Modify Pluto installation/update logic
    Check if Pluto is installed, and update if it is, or install if it isn't.

--- PlutoLauncher.jl
+++ PlutoLauncher.jl
@@ -1,4 +1,15 @@
 using Pkg
-Pkg.add("Pluto")
+pluto_is_installed=false
+for d in values(Pkg.dependencies())
+    if d.name=="Pluto"
+        global pluto_is_installed = true
+        break
+    end
+end
+if pluto_is_installed
+    Pkg.update("Pluto")
+else
+    Pkg.add("Pluto")
+end
 using Pluto
 Pluto.run()
